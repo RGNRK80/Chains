ac4dade31b16cbe27b206153a0cd4ae1543cc4e6
RGNRK80 <armhound@gmail.com>
2021-11-03 10:50:28 +0300

    # 2 complite rotate base

--- src/by/overone/lesson_oxo/Main.java
+++ src/by/overone/lesson_oxo/Main.java
@@ -22,21 +22,35 @@ public class Main {
                 scanner.nextLine();
             continue;}
 
+            //проверка ячейки на соответствие 1-9 и наличие 1 в месте ввода
+
+
+            // ввод ячейки
+            switch (cell) {
+                case 1: {desk+=100; break;}
+                case 2: {desk+=10; break;}
+                case 3: {desk+=1; break;}
+                case 4: {desk+=100000; break;}
+                case 5: {desk+=10000; break;}
+                case 6: {desk+=1000; break;}
+                case 7: {desk+=100000000; break;}
+                case 8: {desk+=10000000; break;}
+                case 9: {desk+=1000000; break;}
+            }
+
+            // сравнение
+
+
 
 
 
         }
-
-
-
-
-
     }
 
     public static void check (){}
 
     public static void showturn (int turn){
-        System.out.println("-------your turn---------");
+        System.out.println("--your turn--");
         char[] deskOfTurn;
         deskOfTurn=Integer.toString(turn).toCharArray();
 
@@ -52,4 +66,24 @@ public class Main {
             }
         }
     }
+
+    public  int rotate (int dsk,int rad){
+        int rezult=-1;
+        String[] dskStr=Integer.toString(dsk).split("");
+        if (rad==0) {rezult=dsk;}
+        if (rad==1) {
+            String rez=dskStr[6]+dskStr[3]+dskStr[0]+dskStr[7]+dskStr[4]+dskStr[1]+dskStr[8]+dskStr[5]+dskStr[2];
+            rezult=Integer.parseInt(rez);}
+        if (rad==2) {
+            String rez=dskStr[8]+dskStr[7]+dskStr[6]+dskStr[5]+dskStr[4]+dskStr[3]+dskStr[2]+dskStr[1]+dskStr[0];
+            rezult=Integer.parseInt(rez);}
+        if (rad==3) {
+            String rez=dskStr[2]+dskStr[5]+dskStr[8]+dskStr[1]+dskStr[4]+dskStr[7]+dskStr[0]+dskStr[3]+dskStr[6];
+            rezult=Integer.parseInt(rez);}
+
+    return rezult;
+    }
+
+
+
 }
